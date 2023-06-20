e45d6dc127584ab709a5e62330939214315e2d29
switzerchees <patrickmichel96@yahoo.de>
2023-06-20 15:45:26 +0200

    Decode the anwer chunks of openai in the backend and paste them to console

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,6 +1,11 @@
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, streamToResponse } from "ai";
 
+const decoder = new TextDecoder();
+export function decodeAIStreamChunk(chunk: Uint8Array): string {
+  return decoder.decode(chunk);
+}
+
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -27,12 +32,15 @@ export default defineEventHandler(async (event: any) => {
   // Respond with the stream
   const reader = stream.getReader();
   return new Promise((resolve, reject) => {
+    let result = "";
     function read() {
       reader.read().then(({ done, value }) => {
         if (done) {
           event.node.res.end();
+          console.log(`Answer from OPENAI: ${result}`);
           return;
         }
+        result += decodeAIStreamChunk(value);
         event.node.res.write(value);
         read();
       });
